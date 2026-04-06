af166f6e3db36eb5250745c04e36f3bc206b8bd1
rafiramadhan <rafirama1020@gmail.com>
2026-04-06 21:53:10 +0700

    Implement factorial, permutation, and combination functions

--- Modul 4 Rafi Ramafdhan/soal1.go
+++ Modul 4 Rafi Ramafdhan/soal1.go
@@ -1,1 +1,27 @@
+package main
 
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func permutation(n, r int) int {
+	return factorial(n) / factorial(n-r)
+}
+
+func combination(n, r int) int {
+	return factorial(n) / (factorial(r) * factorial(n-r))
+}
+
+func main() {
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+
+	fmt.Println(permutation(a, c), combination(a, c))
+
+	fmt.Println(permutation(b, d), combination(b, d))
+}
